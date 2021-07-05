be5993c6788a516b9ccddd2331d1d1f626643d50
AntonioAp <antonioaparicioh@gmail.com>
2021-07-05 22:12:48 +0200

    favicon and .env

--- newsWeb/newsweb/src/components/ListNews/ListNews.js
+++ newsWeb/newsweb/src/components/ListNews/ListNews.js
@@ -20,7 +20,8 @@ class ListNews extends Component {
   componentDidMount() {
     axios
       .get(
-        `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=5&apiKey=c943630bf7ff4e3e812a9874864f71cc`
+          `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=5&apiKey=${process.env.REACT_APP_API_KEY}`
+        //`https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=5&apiKey=c943630bf7ff4e3e812a9874864f71cc`
       )
       .then((res) => {
         const{articles} = res.data;
